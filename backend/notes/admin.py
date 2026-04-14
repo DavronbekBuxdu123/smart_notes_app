915ab388745ad04de4c1bd8d05279c3c5c8d8f99
Aslonov Davronbek <davronaslonov01@gmail.com>
2026-04-14 17:07:17 +0500

    add admin panel

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Category,Note,Tag
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Note)
+admin.site.register(Tag)
